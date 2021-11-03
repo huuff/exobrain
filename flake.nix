da69e6304a982cb8310f66b12d4dba35d50cff25
Haf <haf@protonmail.ch>
2021-11-03 18:44:02 +0100

    adding flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Exobrain - My personal Zettelkasten";
+
+  inputs = {
+    emanote.url = "github:srid/emanote/master";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, emanote, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in with pkgs;
+  {
+    devShell.${system} = mkShell {
+      nativeBuildInputs = [
+        emanote.packages.${system}.project
+      ];
+    };
+  };
+}
